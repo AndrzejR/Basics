35f2beb5f5b0e0de8ce0fb29f44c5c472c3252d2
AndrzejR <andrzej.ruszczewski@gmail.com>
2013-04-29 13:45:28 +0200

    BinaryTree - finished findParent, implemented findNode, changed contains to use findNode.

--- ADS/src/ads/BinaryTree.java
+++ ADS/src/ads/BinaryTree.java
@@ -61,15 +61,18 @@ public class BinaryTree {
 	 * 
 	 * @param value
 	 *            the value to search for in the tree
-	 * @return true if the value is contained in the tree, false otherwise *
+	 * @return True if the value is contained in the tree, false otherwise.
 	 */
 	public boolean contains(int value) {
-		return containsRecursion(value, root);
+		if (findNode(value)!=null){
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	/**
-	 * 
-	 * Main recursive search method.
+	/**	 
+	 * Main recursive search method. Deprecated. ;) Used to be used by contains method.
 	 * 
 	 * @param value
 	 * @param current
@@ -90,14 +93,60 @@ public class BinaryTree {
 	}
 
 	/**
-	 * Finds the parent node of the node containing the given value.
+	 * Recursively finds a reference to the node containing the given value.
+	 * 
+	 * @param value
+	 *            Data of the node to find.
+	 * @return A reference to the node containing the given value, null if the
+	 *         value does not exist in the tree.
+	 */
+	public TreeNode findNode(int value) {
+		return findNodeRecursion(value, root);
+	}
+
+	/**
+	 * Main recursive logic of the findNode method.
 	 * 
 	 * @param value
 	 * @param current
-	 * @return reference to the parent node, null if the value doesn't exist in
-	 *         the tree, or it is contained in the root node
+	 * @return
 	 */
-	private TreeNode findParent(int value, TreeNode current) {
+	private TreeNode findNodeRecursion(int value, TreeNode current) {
+		if (current == null) {
+			return null;
+		} else if (current.data == value) {
+			return current;
+		} else {
+			if (value > current.data) {
+				return findNodeRecursion(value, current.right);
+			} else {
+				return findNodeRecursion(value, current.left);
+			}
+		}
+
+	}
+
+	/**
+	 * Finds the parent node of the node containing the given value.
+	 * 
+	 * @param value
+	 *            Parent of the node containing this value will be returned.
+	 * @return Reference to the parent node, null if the value doesn't exist in
+	 *         the tree, or it is contained in the root node.
+	 */
+	public TreeNode findParent(int value) {
+		return findParentRecursion(value, root);
+	}
+
+	/**
+	 * Main recursive logic for the findParent method.
+	 * 
+	 * @param value
+	 * @param current
+	 * @return Reference to the parent node, null if the value doesn't exist in
+	 *         the tree, or it is contained in the root node.
+	 */
+	private TreeNode findParentRecursion(int value, TreeNode current) {
 		if (root.data == value) {
 			return null;
 		}
@@ -107,7 +156,7 @@ public class BinaryTree {
 			} else if (current.left.data == value) {
 				return current;
 			} else {
-				return findParent(value, current.left);
+				return findParentRecursion(value, current.left);
 			}
 		} else {
 			if (current.right == null) {
@@ -115,7 +164,7 @@ public class BinaryTree {
 			} else if (current.right.data == value) {
 				return current;
 			} else {
-				return findParent(value, current.right);
+				return findParentRecursion(value, current.right);
 			}
 		}
 	}
@@ -138,10 +187,18 @@ public class BinaryTree {
 		System.out.println("13: " + bt.contains(13));
 		System.out.println("666: " + bt.contains(666));
 		System.out.println("123456: " + bt.contains(123456));
-		
-		System.out.println(bt.findParent(42, bt.root));
-		System.out.println(bt.findParent(13, bt.root).data);
-		System.out.println(bt.findParent(10, bt.root).data);
+		System.out.println();
+
+		System.out.println(bt.findParent(42));
+		System.out.println(bt.findParent(13).data);
+		System.out.println(bt.findParent(10).data);
+		System.out.println(bt.findParent(123456));
+		System.out.println();
+
+		System.out.println(bt.findNode(1));
+		System.out.println(bt.findNode(42));
+		System.out.println(bt.findNode(666));
+		System.out.println();
 
 	}
 
